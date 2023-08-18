14bdddcf74004efb2f3fc8f4e5643c2fda264581
Shay yosef <shayagres@gmail.com>
2023-08-18 16:17:37 +0300

    Start work on glowing lines

--- Galcon-Game/Assets/Scripts/TargetGlow.cs
+++ Galcon-Game/Assets/Scripts/TargetGlow.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TargetGlow : MonoBehaviour
 {
     [SerializeField] GameObject _selectedGlow;
+    [SerializeField] GameObject _glowLines;
     public bool _isClicked; // When a planet is clicked, it will glow until it is clicked again or another planet is clicked
     public bool _isSelected; // When a planet is hovered over, it will glow until the mouse exits the planet
     public bool _glowingEnabled = false;
@@ -34,6 +35,7 @@ public class TargetGlow : MonoBehaviour
         }
         else if (this.gameObject.tag == "Enemy" || this.gameObject.tag == "Neutral")
         {
+            GameObject enemy = this.gameObject;
             if (_glowingEnabled)
             {
                 if (_isSelected)
@@ -43,6 +45,7 @@ public class TargetGlow : MonoBehaviour
                     for (int i = 0; i < GameManager.Instance._planets.Count; i++)
                     {
                         Debug.DrawLine(this.transform.position, GameManager.Instance._planets[i].transform.position, Color.red, 1f);
+                        GameObject glowLines = Instantiate(_glowLines, this.transform.position, Quaternion.identity);
                     }
                 }
                 else
