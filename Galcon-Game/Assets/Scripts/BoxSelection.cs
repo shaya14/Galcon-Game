e95b7965d68f634ceed5e11747a2ebbfab7dbbe9
Shay yosef <shayagres@gmail.com>
2023-08-29 21:27:05 +0300

    BoxSelection Work

--- Galcon-Game/Assets/Scripts/BoxSelection.cs
+++ Galcon-Game/Assets/Scripts/BoxSelection.cs
@@ -10,8 +10,9 @@ public class BoxSelection : MonoBehaviour
     private Vector2 _initalMousePosition, _currentMousePosition;
     private BoxCollider2D _boxCollider2D;
     public static BoxSelection _instance;
-
-    public bool _isBoxSelected = false;
+    [SerializeField] private float _selectionTime = 0.2f;
+    private float _timer = 0f;
+    private List<Planet> newlySelectedPlanets = new List<Planet>(); // To store planets newly selected in this frame
 
     void Start()
     {
@@ -37,7 +38,7 @@ public class BoxSelection : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            _isBoxSelected = true;
+            _timer += Time.deltaTime;
             _currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             _lineRenderer.SetPosition(0, new Vector2(_initalMousePosition.x, _initalMousePosition.y));
             _lineRenderer.SetPosition(1, new Vector2(_initalMousePosition.x, _currentMousePosition.y));
@@ -49,7 +50,7 @@ public class BoxSelection : MonoBehaviour
             _boxCollider2D.size = new Vector2(
                 Mathf.Abs(_initalMousePosition.x - _currentMousePosition.x),
                 Mathf.Abs(_initalMousePosition.y - _currentMousePosition.y));
-            if (_isBoxSelected)
+            if (_timer > _selectionTime)
             {
                 SelectObjects();
             }
@@ -57,17 +58,16 @@ public class BoxSelection : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
+            _timer = 0f;
             _lineRenderer.positionCount = 0;
             Destroy(_boxCollider2D);
             transform.position = Vector3.zero;
-            _isBoxSelected = false;
             DrawLines._instance.ClearLines();
         }
     }
 
     public void SelectObjects()
     {
-        List<Planet> newlySelectedPlanets = new List<Planet>(); // To store planets newly selected in this frame
         Collider2D[] objectsToSelect = Physics2D.OverlapBoxAll(transform.position, _boxCollider2D.size, 0);
         foreach (Collider2D selectable in objectsToSelect)
         {
@@ -78,11 +78,10 @@ public class BoxSelection : MonoBehaviour
             }
             if (planet.isFriendly && !planet.isSelected)
             {
-                if (!PlanetManager.Instance._selectedPlanets.Contains(planet))
+                planet.GetComponent<TargetGlow>().SetGlowOn();
+                if (!newlySelectedPlanets.Contains(planet))
                 {
-                    planet.GetComponent<TargetGlow>().SetGlowOn();
-                    PlanetManager.Instance._selectedPlanets.Add(planet);
-                    //newlySelectedPlanets.Add(planet);
+                    newlySelectedPlanets.Add(planet);
                 }
 
                 foreach (Planet enemy in PlanetManager.Instance._enemiesToSelect)
@@ -92,48 +91,16 @@ public class BoxSelection : MonoBehaviour
             }
         }
 
+        // Deselect planets that were selected previously but are not in the newlySelectedPlanets list
         foreach (Planet selectedPlanet in PlanetManager.Instance._selectedPlanets)
         {
-            if (!PlanetManager.Instance._selectedPlanets.Contains(selectedPlanet))
+            if (!newlySelectedPlanets.Contains(selectedPlanet))
             {
                 selectedPlanet.GetComponent<TargetGlow>().SetGlowOff();
             }
         }
+
+        PlanetManager.Instance._selectedPlanets = newlySelectedPlanets; // Update the selected planets list
     }
-
-    // public void SelectObjects()
-    // {
-    //     List<Planet> newlySelectedPlanets = new List<Planet>(); // To store planets newly selected in this frame
-    //     Collider2D[] objectsToSelect = Physics2D.OverlapBoxAll(transform.position, _boxCollider2D.size, 0);
-    //     foreach (Collider2D selectable in objectsToSelect)
-    //     {
-    //         Planet planet = selectable.GetComponent<Planet>();
-    //         if (planet == null)
-    //         {
-    //             continue;
-    //         }
-    //         if (planet.isFriendly && !planet.isSelected)
-    //         {
-    //             planet.GetComponent<TargetGlow>().SetGlowOn();
-    //             newlySelectedPlanets.Add(planet);
-
-    //             foreach (Planet enemy in PlanetManager.Instance._enemiesToSelect)
-    //             {
-    //                 enemy.GetComponent<TargetGlow>()._glowingEnabled = true;
-    //             }
-    //         }
-    //     }
-
-    //     // Deselect planets that were selected previously but are not in the newlySelectedPlanets list
-    //     foreach (Planet selectedPlanet in PlanetManager.Instance._selectedPlanets)
-    //     {
-    //         if (!newlySelectedPlanets.Contains(selectedPlanet))
-    //         {
-    //             selectedPlanet.GetComponent<TargetGlow>().SetGlowOff();
-    //         }
-    //     }
-
-    //     PlanetManager.Instance._selectedPlanets = newlySelectedPlanets; // Update the selected planets list
-    // }
 }
 
